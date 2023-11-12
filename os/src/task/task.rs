3bc17eee4521dc296cf82301f379c42912f95277
H <q>
2023-11-12 11:35:19 +0800

    Finish the fisrt lab in stage 3

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -181,30 +181,61 @@ impl TaskControlBlock {
             .translate(VirtAddr::from(TRAP_CONTEXT_BASE).into())
             .unwrap()
             .ppn();
-        // push arguments on user stack
-        user_sp -= (args.len() + 1) * core::mem::size_of::<usize>();
-        let argv_base = user_sp;
-        let mut argv: Vec<_> = (0..=args.len())
-            .map(|arg| {
-                translated_refmut(
-                    memory_set.token(),
-                    (argv_base + arg * core::mem::size_of::<usize>()) as *mut usize,
-                )
-            })
-            .collect();
-        *argv[args.len()] = 0;
+        // // push arguments on user stack
+        // user_sp -= (args.len() + 1) * core::mem::size_of::<usize>();
+        // let argv_base = user_sp;
+        // let mut argv: Vec<_> = (0..=args.len())
+        //     .map(|arg| {
+        //         translated_refmut(
+        //             memory_set.token(),
+        //             (argv_base + arg * core::mem::size_of::<usize>()) as *mut usize,
+        //         )
+        //     })
+        //     .collect();
+        // *argv[args.len()] = 0;
+        // for i in 0..args.len() {
+        //     user_sp -= args[i].len() + 1;
+        //     *argv[i] = user_sp;
+        //     let mut p = user_sp;
+        //     for c in args[i].as_bytes() {
+        //         *translated_refmut(memory_set.token(), p as *mut u8) = *c;
+        //         p += 1;
+        //     }
+        //     *translated_refmut(memory_set.token(), p as *mut u8) = 0;
+        // }
+        // // make the user_sp aligned to 8B for k210 platform
+        // user_sp -= user_sp % core::mem::size_of::<usize>();
+
+        let mut argv_addr: Vec<usize> = Vec::new();
+        
         for i in 0..args.len() {
             user_sp -= args[i].len() + 1;
-            *argv[i] = user_sp;
+            
             let mut p = user_sp;
+            argv_addr.push(user_sp);
             for c in args[i].as_bytes() {
                 *translated_refmut(memory_set.token(), p as *mut u8) = *c;
                 p += 1;
             }
             *translated_refmut(memory_set.token(), p as *mut u8) = 0;
         }
-        // make the user_sp aligned to 8B for k210 platform
-        user_sp -= user_sp % core::mem::size_of::<usize>();
+
+        user_sp -= (args.len() + 1) * core::mem::size_of::<usize>();
+        let argv_base = user_sp;
+        let mut argv: Vec<_> = (0..=args.len())
+            .map(|arg| {
+                translated_refmut(
+                    memory_set.token(), 
+                    (argv_base + arg * core::mem::size_of::<usize>()) as *mut usize,)
+            })
+            .collect();
+        for i in 0..args.len() {
+            *argv[i] = argv_addr[i];
+        }
+        *argv[args.len()] = 0;
+        user_sp -= core::mem::size_of::<usize>();
+        *translated_refmut(memory_set.token(), user_sp as *mut usize) = args.len();
+
 
         // **** access current TCB exclusively
         let mut inner = self.inner_exclusive_access();
